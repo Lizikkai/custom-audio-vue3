b997bbe1d7f03805530fea1f1c5f850eed7e563a
lizikai <lzk9863@126.com>
2025-09-19 11:07:43 +0800

    修复vue找不到模块

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,12 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+// Vue module declaration
+declare module "vue" {
+  export * from "@vue/runtime-dom";
+}
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
